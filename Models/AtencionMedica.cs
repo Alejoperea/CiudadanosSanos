7087c82f6c290230e2c6f03756f00882c2c60636
Alejo Perea <mpereamejia@gmail.com>
2023-11-18 20:49:31 -0500

    Creacion de clases

---
+++ Models/AtencionMedica.cs
@@ -0,0 +1,21 @@
+﻿namespace CiudadanosSanos.Models
+{
+	public class AtencionMedica
+	{
+		//[Key] -> Anotacion si la propiedad no se llama Id ejemplo ProductId
+		public int Id { get; set; } //Seria la llave primaria 
+		public string NameMedico { get; set; }
+
+		public string NamePaciente { get; set; }
+
+		public string Fecha { get; set; }
+
+		public int PacienteId { get; set; } // sera la llave foranea 
+
+		public ICollection<Paciente>? Pacientes { get; set; } = default!;    // Propiedad de navegacion 
+
+
+
+
+	}
+}
---
+++ Models/Paciente.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace CiudadanosSanos.Models
+{
+	public class Paciente
+	{
+		public int Id { get; set; } // Llave primaria 
+
+		public string Name { get; set; }
+
+		public int Edad {  get; set; }
+		public string Sexo { get; set; }	
+
+		public ICollection<AtencionMedica> AtencionMedicas { get; set; }
+	}
+}
