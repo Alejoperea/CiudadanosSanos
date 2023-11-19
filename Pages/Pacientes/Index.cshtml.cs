1193e19d039a30a17efa0e9cf0a7813650d8d785
Alejo Perea <mpereamejia@gmail.com>
2023-11-18 21:57:49 -0500

    Index Pacientes

---
+++ Pages/Pacientes/Index.cshtml.cs
@@ -0,0 +1,27 @@
+using CiudadanosSanos.Data;
+using CiudadanosSanos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace CiudadanosSanos.Pages.Pacientes
+{
+    public class IndexModel : PageModel
+    {
+		private readonly CiudadanosSanosContext _context;
+		public IndexModel(CiudadanosSanosContext context)
+		{
+
+			_context = context;
+		}
+		public IList<Paciente> Pacientes { get; set; } = default!;
+
+		public async Task OnGetAsync()
+		{
+			if (_context.Pacientes != null)
+			{
+				Pacientes = await _context.Pacientes.ToListAsync();
+			}
+		}
+	}
+}
